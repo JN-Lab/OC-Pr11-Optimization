4571dfe204e8cf3c9d57b69fca4c6d6c51ec656f
juliennuellas <julien.nuellas@gmail.com>
2018-08-16 15:30:34 +0200

    first version from registration with email registration -> test

--- search/views.py
+++ search/views.py
@@ -8,6 +8,13 @@ from .forms import HeaderSearchForm, HomeSearchForm, RegisterForm, ConnexionForm
 from .utils.treatment import Treatment
 from .models import Product, Category, Profile
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+
 # Create your views here.
 
 # Home page
@@ -147,10 +154,28 @@ def register(request):
 
             username_already_exist = User.objects.filter(username=username).exists()
             if not username_already_exist and password == password_check:
-                user = User.objects.create_user(username, mail, password)
+                user = User.objects.create_user(username, mail, password, is_active=False)
                 user_profile = Profile(user=user)
                 user_profile.save()
-                return redirect(reverse('log_in'), locals())
+
+                current_site = get_current_site(request)
+                mail_subject = 'Activer votre compte.'
+                message = render_to_string('acc_activate_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = mail
+                email = EmailMessage(
+                    mail_subject,
+                    message,
+                    to=[to_email]
+                )
+                email.send()
+                return HttpResponse("""S'il vous plaît, confirmez votre adresse mail
+                    pour finaliser votre inscription""")
+
             else:
                 error = True
                 if username_already_exist:
@@ -162,6 +187,21 @@ def register(request):
         register_form = RegisterForm()
         return render(request, 'register.html', locals())
 
+# Activation view for registration
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+        
 # Log-in
 def log_in(request):
     """
