199d33613ffd028ab0d1faf42d2c7907b7e23e8f
juliennuellas <julien.nuellas@gmail.com>
2018-08-17 09:35:26 +0200

    add an association between messages level and bootstrap classes

--- purbeurre_platform/settings.py
+++ purbeurre_platform/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -168,3 +169,13 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dev.openclassrooms@gmail.com'
 EMAIL_HOST_PASSWORD = 'dev_openclassrooms_2018'
 EMAIL_PORT = 587
+
+# Messages configuration with boostrap class
+
+MESSAGES_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
